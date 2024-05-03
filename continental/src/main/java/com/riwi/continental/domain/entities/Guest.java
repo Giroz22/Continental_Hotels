c469f2379b2f00083f51daaea1cd916ea6e7da52
mtatiana115 <mtatiana115@gmail.com>
2024-05-03 11:05:41 -0500

    fix/fetch added

--- continental/src/main/java/com/riwi/continental/domain/entities/Guest.java
+++ continental/src/main/java/com/riwi/continental/domain/entities/Guest.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +38,7 @@ private int age;
 private AgeCategory ageCategory;
 
 
-@ManyToOne
+@ManyToOne(fetch = FetchType.LAZY)
 @JoinColumn(name = "booking_id" , referencedColumnName = "id")
 private Booking booking;
 
