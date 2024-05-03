06326fe23d54c4d0a5efc21260065387d7c2b1db
mtatiana115 <mtatiana115@gmail.com>
2024-05-03 13:35:47 -0500

    feature/Repo interface & service added to customer

---
+++ continental/src/main/java/com/riwi/continental/infrastructure/services/CustomerService.java
@@ -0,0 +1,62 @@
+package com.riwi.continental.infrastructure.services;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.riwi.continental.api.dto.request.CustomerRequest;
+import com.riwi.continental.api.dto.response.CustomerResponse;
+import com.riwi.continental.domain.entities.Customer;
+import com.riwi.continental.domain.repositories.CustomerRepository;
+import com.riwi.continental.infrastructure.abstract_services.ICustomerService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CustomerService implements ICustomerService{
+
+  @Autowired
+  private final CustomerRepository customerRepository;
+
+  @Override
+  public Page<CustomerResponse> getAll(int page, int size) {
+    if(page < 0) page = 0;
+
+    PageRequest pagination = PageRequest.of(page, size);
+    return this.customerRepository.findAll(pagination).map(customer -> this.entityToResponse(customer));
+  }
+
+  @Override
+  public CustomerResponse findById(String id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'findById'");
+  }
+
+  @Override
+  public CustomerResponse create(CustomerRequest entity) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'create'");
+  }
+
+  @Override
+  public CustomerResponse update(CustomerRequest entity) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'update'");
+  }
+
+  @Override
+  public void delete(String id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'delete'");
+  }
+
+  private CustomerResponse entityToResponse(Customer entity){
+    CustomerResponse response = new CustomerResponse();
+    BeanUtils.copyProperties(entity, response);
+    return response;
+  }
+
+}
