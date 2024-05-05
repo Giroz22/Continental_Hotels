3f4cd5a091f09fe52f4d3bbcac6b034d830c51f8
mtatiana115 <mtatiana115@gmail.com>
2024-05-05 10:35:07 -0500

    fix/id added to response customer

--- continental/src/main/java/com/riwi/continental/api/dto/response/CustomerResponse.java
+++ continental/src/main/java/com/riwi/continental/api/dto/response/CustomerResponse.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerResponse {
+  private String id;
   private String name;
   private String lastname;
   private int age;
