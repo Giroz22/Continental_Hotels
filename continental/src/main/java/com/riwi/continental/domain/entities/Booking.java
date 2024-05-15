c7b74c7e9e0a06e375d76a61be04778c71745549
EstebanBedoyaHiguita <estebanbedoyahiguita@gmail.com>
2024-05-15 17:06:45 -0500

    validations and Audit of booking it's ready

--- continental/src/main/java/com/riwi/continental/domain/entities/Booking.java
+++ continental/src/main/java/com/riwi/continental/domain/entities/Booking.java
@@ -1,11 +1,19 @@
 package com.riwi.continental.domain.entities;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,16 +31,26 @@ import lombok.ToString;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    @Column(nullable = false)
     private LocalDate admissionDate;
+    @Column(nullable = false)
     private LocalDate departureDate;
+    @Column(nullable = false)
     private LocalTime admissionTime;
+    @Column(nullable = false)
     private LocalTime departureTime;
+
+    @Column(name="modified_date")
+    @LastModifiedDate
+    @JsonIgnore
+    private LocalDateTime modifiedDate;
     
     @ManyToOne
     @JoinColumn(name = "id_customer", referencedColumnName = "id")
