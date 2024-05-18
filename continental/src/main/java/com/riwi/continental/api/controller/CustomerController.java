08facb9772e82587a96ea97e20eb5756f78dea14
EstebanBedoyaHiguita <estebanbedoyahiguita@gmail.com>
2024-05-17 20:06:49 -0500

    Documentation of booking with swagger

--- continental/src/main/java/com/riwi/continental/api/controller/CustomerController.java
+++ continental/src/main/java/com/riwi/continental/api/controller/CustomerController.java
@@ -13,10 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.continental.api.dto.errors.ErrorResponse;
+import com.riwi.continental.api.dto.errors.ErrorsResponse;
 import com.riwi.continental.api.dto.request.CustomerRequest;
 import com.riwi.continental.api.dto.response.CustomerResponse;
 import com.riwi.continental.infrastructure.abstract_services.ICustomerService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -25,23 +31,35 @@ import lombok.AllArgsConstructor;
 public class CustomerController {
   private final ICustomerService iCustomerService;
 
+  @Operation(summary = "Get all the list of bookings in paginated form")
+    @ApiResponse(responseCode = "400", description = "When the connection with the data base fail", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorsResponse.class))})
   @GetMapping
   public ResponseEntity<Page<CustomerResponse>> getAll(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "3") int size){
     return ResponseEntity.ok(this.iCustomerService.getAll(page - 1, size));
   }
 
+  @Operation(summary = "This method create a booking with the dates sent")
+    @ApiResponse(responseCode = "400", description = "When there is an error in the date sent to the datebase", content = {
+        @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorsResponse.class))})
   @PostMapping
   public ResponseEntity<CustomerResponse> insert (
     @Validated @RequestBody CustomerRequest customer){
       return ResponseEntity.ok(this.iCustomerService.create(customer));
     }
 
+  @Operation(summary = "Get all the bookings find with a id")
+  @ApiResponse(responseCode = "400", description = "When the id is not valid", content = {
+      @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})
   @GetMapping(path = "/{id}")
   public ResponseEntity<CustomerResponse> get(
     @PathVariable String id ){
       return ResponseEntity.ok(this.iCustomerService.findById(id));
     }
 
+  @Operation(summary = "This method allows you delete a booking for a id especific")
+  @ApiResponse(responseCode = "400", description = "When the id it's not valid", content = {
+      @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})
   @DeleteMapping(path = "/{id}")
   public ResponseEntity<Void> delete (
     @PathVariable String id){
@@ -49,6 +67,9 @@ public class CustomerController {
       return ResponseEntity.noContent().build();
     }  
   
+  @Operation(summary = "This method allows you modify a booking for a id especific")
+  @ApiResponse(responseCode = "400", description = "When the id it's not valid", content = {
+      @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})
     @PutMapping(path = "/{id}")
     public ResponseEntity<CustomerResponse> update(
       @Validated @PathVariable String id,
